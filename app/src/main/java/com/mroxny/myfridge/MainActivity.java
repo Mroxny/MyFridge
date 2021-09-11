ea9462cc55361ab6cdacad508b07b6b710daef71
Mroxny <50219282+Mroxny@users.noreply.github.com>
2021-09-11 13:59:04 +0200

    Project set up

---
+++ app/src/main/java/com/mroxny/myfridge/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mroxny.myfridge;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+}
